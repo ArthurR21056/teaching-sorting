c13ad54e3be4f58f125fac6490293a2a2118a381
Art Reeves III <arthur@Arts-MacBook-Pro.local>
2019-12-04 09:50:06 -0600

    Created React App to teach people sorting algorithms

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import './header.styles.scss'
+
+const Header = () => (
+    <div className='header'> 
+        <div className='options'>
+            <div className='option'>
+                <h2>Add Card</h2> <input type='Number'/>
+            </div>
+            <div className='option'>
+            
+            </div>
+            
+        </div>
+    </div>
+);
+
+export default Header
